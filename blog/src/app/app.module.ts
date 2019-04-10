67f59719c150e0439aebf5e171699942594ad639
miłosz lazarowicz <miloszlazar@gmail.com>
2019-04-10 12:49:27 +0200

    'changes'

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,34 +1,54 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./components/home/home.component";
-import {ContactComponent} from "./components/contact/contact.component";
-import {BlogComponent} from "./components/blog/blog.component";
-import {QuizComponent} from "./components/quiz/quiz.component";
-import { NavbarComponent } from './navbar/navbar.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'contact',
-    component: ContactComponent,
-  },
-  {
-    path: 'blog',
-    component: BlogComponent,
-  },
-  {
-    path: 'quiz',
-    component: QuizComponent,
-  }];
-
-
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {BlogComponent} from './components/blog/blog.component';
+import {QuizComponent} from './components/quiz/quiz.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {BlogItemComponent} from './components/blog-item/blog-item.component';
+import {BlogItemTextComponent} from './components/blog-item-text/blog-item-text.component';
+import {BlogItemImageComponent} from './components/blog-item-image/blog-item-image.component';
+import {BlogItemDetailsComponent} from './components/blog-item-details/blog-item-details.component';
+import {HttpClientModule} from "@angular/common/http";
+import {DataService} from "./services/data.service";
+import {SummaryPipe} from './pipes/summary.pipe';
+import {FormsModule} from "@angular/forms";
+import {SearchBarComponent} from './shared/search-bar/search-bar.component';
+import {BlogHomeComponent} from './components/blog-home/blog-home.component';
+import {FilterPipe} from './pipes/filter.pipe';
+import {TextFormatDirective} from './directives/text-format.directive';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  declarations: [NavbarComponent]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    QuizComponent,
+    ContactComponent,
+    NavbarComponent,
+    BlogItemComponent,
+    BlogItemTextComponent,
+    BlogItemImageComponent,
+    BlogItemDetailsComponent,
+    SummaryPipe,
+    SearchBarComponent,
+    BlogHomeComponent,
+    FilterPipe,
+    TextFormatDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [
+    DataService
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppRoutingModule { }
+export class AppModule {
+}
